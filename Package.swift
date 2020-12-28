955eaffbf9e17fc24fac198f6af3ba5bca4b9f15
octopused <octopused@gmail.com>
2020-12-28 16:54:34 +0500

    utilities product

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "XsollaUtilities",
+            targets: ["XsollaUtilities"]),
+        .library(
             name: "XsollaLoginKit",
             targets: ["XsollaLoginKit"]),
         .library(
