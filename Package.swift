1c540b5c942dc48a250c59dff2c8ea2cc61486d4
octopused <octopused@gmail.com>
2020-12-28 18:00:04 +0500

    г

--- Package.swift
+++ Package.swift
@@ -28,7 +28,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "XsollaUtilities"),
+            name: "XsollaUtilities",
+            path: "Utilities"),
         
         .target(
             name: "XsollaLoginKit",
